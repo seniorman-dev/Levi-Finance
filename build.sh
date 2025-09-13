2662f95ab67434ebb7d27929a39979bfd90c28da
seniorman-dev <coder.jay2001@gmail.com>
2025-09-13 22:40:01 +0100

    modified:   levi/levi/wsgi.py

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
+
+
+
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Set Python path to include the main directory
+export PYTHONPATH=/opt/render/project/src
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
 
+# Apply database migrations
 python manage.py migrate
--- levi/levi/wsgi.py
+++ levi/levi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'levi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'levi.levi.settings')
 
 application = get_wsgi_application()
