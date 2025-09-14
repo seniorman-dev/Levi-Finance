79805fb64710094685bf43b7d87177e9dc30f8b7
seniorman-dev <coder.jay2001@gmail.com>
2025-09-14 09:25:40 +0100

    modified:   levi/levi/settings.py

--- levi/levi/settings.py
+++ levi/levi/settings.py
@@ -19,6 +19,9 @@ from decouple import config
 from datetime import timedelta
 import dj_database_url
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,15 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-ziob($b1bpljb_q=3_+&n@tfc)r-f8px5e2ryrrrw#8$473nfv'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-
 # Allow all hosts for now, or specify your Render domain
 ALLOWED_HOSTS = ['levi-finance.onrender.com', 'localhost', '127.0.0.1']
-
 # OR for development, you can allow all hosts (not recommended for production)
 #ALLOWED_HOSTS = ["*"]
 
